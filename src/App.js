9eb3193fadfa75f88912515383e3f8237ea72bd3
Dru Serkes <DruSerkes@gmail.com>
2020-08-07 15:04:23 -0400

    Install react-router-dom. Wrap Browser Router around Routes component in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Testing</h1>
+			<BrowserRouter>
+				<Routes />
+			</BrowserRouter>
 			{/* Wrap Browser Router around app in index */}
 			{/* Render Routes Component here  */}
 		</div>
---
+++ src/Routes.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+const Routes = () => {
+
+}
