5604393f30bace4d9a11a4e911adfac55e7a2e6d
Dru Serkes <DruSerkes@gmail.com>
2020-08-07 12:53:33 -0400

    Add proxy. Add scripts: server, client, dev

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
 	return (
 		<div className="App">
+			<h1>Testing</h1>
 			{/* Wrap Browser Router around app in index */}
 			{/* Render Routes Component here  */}
 		</div>
