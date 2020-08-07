e737556aff5c853bb098172221a749ac06cccf63
Dru Serkes <DruSerkes@gmail.com>
2020-08-07 15:13:26 -0400

    Add routing for Routes component with component placeholders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
+// import Navbar
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 		<div className="App">
 			<h1>Testing</h1>
 			<BrowserRouter>
+				{/* <Navbar /> */}
 				<Routes />
 			</BrowserRouter>
 			{/* Wrap Browser Router around app in index */}
--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,36 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 const Routes = () => {
+	return (
+		<Switch>
+			<Route exact path="/jobs">
+				{/* <Jobs /> */}
+				<h1>This is the Jobs page</h1>
+			</Route>
+			<Route exact path="/companies/:handle">
+				{/* <Company /> */}
+				<h1>This is the Company page</h1>
+			</Route>
+			<Route exact path="/companies">
+				{/* <Companies /> */}
+				<h1>This is the Company page</h1>
+			</Route>
+			<Route exact path="/login">
+				{/* <Login /> */}
+				<h1>This is the Login page</h1>
+			</Route>
+			<Route exact path="/profile">
+				{/* <Profile /> */}
+				<h1>This is the Profile page</h1>
+			</Route>
+			<Route exact path="/">
+				{/* <Home /> */}
+				<h1>This is the Home page</h1>
+			</Route>
+			<Redirect path="/" to="/" />
+		</Switch>
+	);
+};
 
-}
+export default Routes;
