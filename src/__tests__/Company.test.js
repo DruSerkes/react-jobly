8cd32c1419eb8ae929085bf0d720097c4cf689a7
Dru Serkes <DruSerkes@gmail.com>
2020-08-08 02:44:19 -0400

    write smoke and snapshot tests for Company

--- src/__tests__/Company.test.js
+++ src/__tests__/Company.test.js
@@ -7,20 +7,30 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Company from '../Company';
 import { MemoryRouter, Route } from 'react-router-dom';
+import { useParams } from 'react-router';
 import JoblyApi from '../JoblyApi';
 
 describe('Company tests', () => {
 	it('renders without crashing', () => {
-		render(<Company />);
+		jest.mock('react-router', () => ({
+			useParams : jest.fn().mockReturnValue({ handle: 'anderson-arias-and-morrow' })
+		}));
+
+		render(
+			<MemoryRouter initialEntries={[ '/companies/anderson-arias-and-morrow' ]}>
+				<Route exact path="/companies/:handle">
+					<Company />
+				</Route>
+			</MemoryRouter>
+		);
 	});
 
-	it('Shows a company', () => {
-		// const mockCompany = jest
-		// 	.spyOn(JoblyApi, getCompany('test'))
-		// 	.mockImplementation(() => ({ handle: 'test', name: 'test', description: 'it is a test' }));
-		// const mockHandle = jest.spyOn(useParams).mockImplementation(() => ({ test }));
-		jest.mock('react-router-dom', () => ({
+	it('matches snapshot', () => {
+		jest.mock('react-router', () => ({
 			useParams : jest.fn().mockReturnValue({ handle: 'anderson-arias-and-morrow' })
+		}));
+		jest.mock('../JoblyApi', () => ({
+			getCompany : jest.fn().mockReturnValue({ name: 'Anderson', description: 'testing them' })
 		}));
 		const { getByText, asFragment } = render(
 			<MemoryRouter initialEntries={[ '/companies/anderson-arias-and-morrow' ]}>
@@ -30,5 +40,6 @@ describe('Company tests', () => {
 			</MemoryRouter>
 		);
 		expect(asFragment()).toMatchSnapshot();
+		// expect(getByText('Anderson Arias and Morrow')).toBeInTheDocument();
 	});
 });
